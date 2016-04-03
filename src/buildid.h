0c5dac7d77dd5cbd2a4400d6dc91884e1e51fc17
Derakon <derakon@gmail.com>
2016-04-02 18:29:52 -0700

    Change program name, version number.

--- src/buildid.h
+++ src/buildid.h
@@ -19,14 +19,14 @@
 #ifndef BUILDID
 #define BUILDID
 
-#define VERSION_NAME	"Angband"
+#define VERSION_NAME	"Rocketband"
 
 #ifdef BUILD_ID
 # define STR(x) #x
 # define XSTR(x) STR(x)
 # define VERSION_STRING XSTR(BUILD_ID)
 #else
-# define VERSION_STRING "4.0.4"
+# define VERSION_STRING "10000.0"
 #endif
 
 extern const char *buildid;
